a39cd8fff93861986e0b9c4e99e218a0af57927c
Valerii Shmidt <valeriyshmidt@gmail.com>
2023-04-30 18:24:50 +0300

    Added comments and XML documentation

--- TechSuportTest/Iizha4ockTest.cs
+++ TechSuportTest/Iizha4ockTest.cs
@@ -1,12 +1,35 @@
-﻿namespace TechSuportTest
+﻿/// <summary>
+/// Provides a static method for calculating the minimum count
+/// of rendezvous of hedgehogs
+/// </summary>
+
+namespace TechSuportTest
 {
     public static class Iizha4ockTest
     {
+        /// <summary>
+        /// Calculates the minimum count of rendezvous of hedgehogs
+        /// </summary>
+        /// <param name="selectedColor">
+        /// The index of the selected color (0 for Red, 1 for Green, 2 for Blue).
+        /// </param>
+        /// <param name="countsByColor">
+        /// An array containing the count of each color (Red, Green, Blue) in a certain situation.
+        /// </param>
+        /// <returns>
+        /// The minimum count of rendezvous required to change colors of hedgehogs to selected one.
+        /// If there is no way to reach the target returns -1.
+        /// </returns>
         public static int CalculateMinCountOfRendezvous(int selectedColor, int[] countsByColor)
         {
+            // Declaration of variables
+
             int selectedColorCount;
             int firstNonSelectedColorCount;
             int secondNonSelectedColorCount;
+
+            // Initialization of variables.
+            // firstNonSelectedColorCount is not smaller than secondNonSelectedColorCount
             if (selectedColor == 0)
             {
                 selectedColorCount  = countsByColor[0];
@@ -28,7 +51,7 @@
                 secondNonSelectedColorCount = Math.Min(countsByColor[0], countsByColor[1]);
             }
 
-
+            // Checking for possibility to reach the target
             if ((firstNonSelectedColorCount - secondNonSelectedColorCount) % 3 != 0)
             {
                 return -1;
@@ -39,28 +62,35 @@
                 return -1;
             }
 
+            // Main loop of program. Continue executing the loop until
+            // the counts of hedgehogs of two non-selected colors became equal.
             int countOfRendezvous = 0;
-
             while (firstNonSelectedColorCount - secondNonSelectedColorCount != 0)
             {
+                // If there are no hedgehogs of the selected color left, increase their count
                 if (selectedColorCount == 0)
                 {
                     firstNonSelectedColorCount -= 1;
                     secondNonSelectedColorCount -= 1;
                     selectedColorCount += 2;
-                    
+
+                    // Increment the count of rendezvous.
                     countOfRendezvous++;
                 }
 
+                // Increase the count of the second non-selected color (the smaller one)
                 firstNonSelectedColorCount -= 1;
                 secondNonSelectedColorCount += 2;
                 selectedColorCount -= 1;
 
+                // Increment the count of rendezvous.
                 countOfRendezvous++;
             }
 
+            // Increase the count rendezvous by count of hedgehogd of non-selected color left
             countOfRendezvous += firstNonSelectedColorCount;
 
+            // Return the result
             return countOfRendezvous;
         }       
     }
--- TechSuportTest/SudokuTests.js
+++ TechSuportTest/SudokuTests.js
@@ -1,4 +1,11 @@
+/**
+Determines if the given 2D array represents a valid Sudoku solution.
+@param {Array<Array<number>>} solution - The Sudoku solution to be validated.
+@returns {boolean} - True if the solution is valid, false otherwise.
+*/
+
 function validSolution(solution) {
+    // Check for repeated values in the row or column.
     for (let i = 0; i < 9; i++) {
         const row = new Set();
         const col = new Set();
@@ -12,12 +19,13 @@ function validSolution(solution) {
                 return false;
             }
 
+            // Add values to the row and column Sets.
             row.add(solution[i][j]);
             col.add(solution[j][i]);
         }
     }
 
-
+    // Check for repeated values in the 3*3 squares
     for (let i = 0; i < 9; i += 3) {
         for (let j = 0; j < 9; j += 3) {
             const square = new Set();
@@ -30,11 +38,14 @@ function validSolution(solution) {
                         return false;
                     }
 
+                    // Add values to the square Set.
                     square.add(value);
                 }
             }
         }
     }
 
+    // If all checks pass, the solution is valid.
     return true;
 }
+
